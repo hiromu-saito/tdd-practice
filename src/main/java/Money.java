c5417449b056c4cae2333fa03690ea4328a017bc
hiromu_saito <hiromu.saito@bigtreetc.com>
2021-08-08 00:12:12 +0900

    chapter 13

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
-public class Money implements Expression{
+public class Money implements Expression {
     protected int amount;
     protected String currency;
 
@@ -34,6 +34,10 @@ public class Money implements Expression{
     }
 
     Expression plus(Money addend) {
-        return new Money(amount + addend.amount, this.currency);
+        return new Sum(this, addend);
+    }
+
+    public Money reduce(String to) {
+        return this;
     }
 }
